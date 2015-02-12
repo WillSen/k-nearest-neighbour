a9745fb79a8b20ce4db6db17681f9ae5a8181e81
Will Sentance <william.sentance@gmail.com>
2015-02-12 12:23:07 -0800

    adds calculateranges

--- public/index.js
+++ public/index.js
@@ -12,3 +12,31 @@ var NodeList = function(k){
 NodeList.prototype.add = function(node) {
     this.nodes.push(node);
 };
+
+NodeList.prototype.calculateRanges = function() {
+  this.areas = {min: 1000000, max: 0};
+  this.rooms = {min: 1000000, max: 0};
+  for (var i in this.nodes)
+  {
+    if (this.nodes[i].rooms < this.rooms.min)
+    {
+        this.rooms.min = this.nodes[i].rooms;
+    }
+
+    if (this.nodes[i].rooms > this.rooms.max)
+    {
+        this.rooms.max = this.nodes[i].rooms;
+    }
+
+    if (this.nodes[i].area < this.areas.min)
+    {
+        this.areas.min = this.nodes[i].area;
+    }
+
+    if (this.nodes[i].area > this.areas.max)
+    {
+        this.areas.max = this.nodes[i].area;
+    }
+  }
+
+};
